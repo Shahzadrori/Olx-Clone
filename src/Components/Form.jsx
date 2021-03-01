cdc3f4628fd7b310071e0b6af8c10b5ef4bb1b8c
shahzadrori <shahzadrori@gmail.com>
2021-03-01 16:28:30 +0500

    update

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,21 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../Style/form.css';
+import {useState} from 'react';
 
 const Form =()=>{
+    const [data, setdata] = useState({
+        fname:'',
+        lname:'',
+        email:'',
+        password:''
+    });
+    const targetval=(event)=>{
+        const value=event.target.value;
+        const name
+    }
+
     return(
         <>
             <div className='outer'>
                     <div className='inner'>
-                    <i className="fas fa-times"/>
+                   <Link to='/'><i className="fas fa-times"/></Link>
                         <h1 className='top'>Login</h1>
-                        <label className='fname'>First Name</label>
-                        <input className='inp'  type='text' placeholder='Enter your first name'/>
-                        <label className='lname'>Last Name</label>
-                        <input className='inp'  type='text' placeholder='Enter your last name'/>
-                        <label  className='email'>Email</label>
-                        <input className='inp'  type='text' placeholder='Enter your email'/>
-                        <label className='password'>Password</label>
-                        <input className='inp'  type='password' placeholder='Enter the password' />
+                        <label>First Name</label>
+                        <input className='inp' name='fname' type='text' placeholder='Enter your first name'/>
+                        <label>Last Name</label>
+                        <input className='inp' name='lname' type='text' placeholder='Enter your last name'/>
+                        <label>Email</label>
+                        <input className='inp' name='email' type='text' placeholder='Enter your email'/>
+                        <label>Password</label>
+                        <input className='inp' name='password' type='password' placeholder='Enter the password' />
                         <div className='btns'>
                         <button className='sub-btn'>Submit</button>
                         <button className='res-btn'>Reset</button>
