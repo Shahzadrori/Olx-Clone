5339627cbe975776404d470ec4687b90f56e01c1
shahzadrori <shahzadrori@gmail.com>
2021-03-02 10:39:53 +0500

    writing the script for the inputs

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -10,9 +10,58 @@ const Form =()=>{
         email:'',
         password:''
     });
+    const [info,setinfo] = useState();
+    console.log(data)
+    let response = [];
+    const Sub=()=>{
+    let finaldata = response.push(data);
+    }
+    // const Sub=(dato=data)=>{
+    //  var recievedata = JSON.stringify(dato);
+    //  response.push(recievedata);
+    //  alert(response)
+    //  localStorage.setItem('session',response)
+
+    // }
     const targetval=(event)=>{
-        const value=event.target.value;
-        const name
+        const value = event.target.value;
+        const name = event.target.name;
+        // console.log(value + name);
+        setdata((prevalue)=>{
+            if(name == 'fname'){
+                return{
+                    fname:value,
+                    lname:prevalue.lname,
+                    email:prevalue.email,
+                    password:prevalue.password
+                }
+            }
+            else if(name == 'lname'){
+                return{
+                    fname:prevalue.fname,
+                    lname:value,
+                    email:prevalue.email,
+                    password:prevalue.password
+                }
+            }
+            else if(name == "email"){
+                return{
+                    fname:prevalue.fname,
+                    lname:prevalue.lname,
+                    email:value,
+                    password:prevalue.password
+                }
+            }
+            else if(name == 'password'){
+                return{
+                    fname:prevalue.fname,
+                    lname:prevalue.lname,
+                    email:prevalue.email,
+                    password:value
+                }
+            }
+            console.log(prevalue)
+        })
     }
 
     return(
@@ -22,15 +71,15 @@ const Form =()=>{
                    <Link to='/'><i className="fas fa-times"/></Link>
                         <h1 className='top'>Login</h1>
                         <label>First Name</label>
-                        <input className='inp' name='fname' type='text' placeholder='Enter your first name'/>
+                        <input value={data.fname} onChange={targetval} className='inp' name='fname' type='text' placeholder='Enter your first name'/>
                         <label>Last Name</label>
-                        <input className='inp' name='lname' type='text' placeholder='Enter your last name'/>
+                        <input value={data.lname} onChange={targetval} className='inp' name='lname' type='text' placeholder='Enter your last name'/>
                         <label>Email</label>
-                        <input className='inp' name='email' type='text' placeholder='Enter your email'/>
+                        <input value={data.email} onChange={targetval} className='inp' name='email' type='text' placeholder='Enter your email'/>
                         <label>Password</label>
-                        <input className='inp' name='password' type='password' placeholder='Enter the password' />
+                        <input value={data.password} onChange={targetval} className='inp' name='password' type='password' placeholder='Enter the password' />
                         <div className='btns'>
-                        <button className='sub-btn'>Submit</button>
+                        <button onClick={Sub} className='sub-btn'>Submit</button>
                         <button className='res-btn'>Reset</button>
                         </div>
                     </div>
