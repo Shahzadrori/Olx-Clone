108e7363dc00ec2c67ecb546535a8eba683199ea
shahzadrori <shahzadrori@gmail.com>
2021-03-03 11:47:22 +0500

    styling the link div

---
+++ src/Components/Links.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "../Style/linkdiv.css";
+const Lindiv = () => {
+  return (
+    <>
+        <div className="links">
+          <a href="#">Mobile Phones</a>
+          <a href="#">Cars</a>
+          <a href="#">Motor Cycles</a>
+          <a href="#">Houses</a>
+          <a href="#">TV</a>
+          <a href="#">Tablets</a>
+          <a href="#">Land and Plots</a>
+        </div>
+        <div className='add'>
+            <div className='popsearch'>
+                <a href="#"></a> - 
+                <a href="#"></a> - 
+                <a href="#"></a> - 
+                <a href="#"></a> - 
+                <a href="#"></a> - 
+                <a href="#"></a> -
+            </div>
+      </div>
+    </>
+  );
+};
+export default Lindiv;
